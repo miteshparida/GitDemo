b8d2d6498a134cbebb9580de881534afaad3d738
miteshparida <miteshparida89@gmail.com>
2019-03-10 22:30:27 +0530

    commit added to git

---
+++ src/test/java/UdemyTutorials/MyTestSuite1Test.java
@@ -0,0 +1,20 @@
+package UdemyTutorials;
+
+import org.testng.annotations.Test;
+
+public class MyTestSuite1Test 
+
+{
+	@Test
+	public void webservice1()
+	{
+		System.out.println("running webservice1 method");
+	}
+	
+	@Test
+	public void webservice2()
+	{
+		System.out.println("running webservice1 method");
+	}
+
+}
---
+++ src/test/java/UdemyTutorials/MyTestSuite2Test.java
@@ -0,0 +1,20 @@
+package UdemyTutorials;
+
+import org.testng.annotations.Test;
+
+public class MyTestSuite2Test 
+
+{
+	@Test
+	public void mobileapp1()
+	{
+		System.out.println("running mobileapp1 method");
+	}
+	
+	@Test
+	public void mobileapp2()
+	{
+		System.out.println("running mobileapp2 method");
+	}
+
+}
